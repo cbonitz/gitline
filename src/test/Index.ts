3d2bc5c8d482d714d18a300106d68132c10f4284
Christoph Bonitz <ml.christophbonitz@gmail.com>
2015-11-08 11:15:47 +0100

    failed attempt

---
+++ src/test/Index.ts
@@ -0,0 +1,10 @@
+/// <reference path="../../typings/tsd.d.ts" />
+import chai = require('chai');
+import fs = require('fs');
+var expect = chai.expect;
+
+describe('Smoke Test:', () => {
+  it('should say hello world', () => {
+
+  });
+});
